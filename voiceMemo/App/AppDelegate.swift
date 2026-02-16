e6f9c9d48f714d4383c63e06ccfd9314ed0d42b6
Junsung_Lee <brian9777@naver.com>
2026-02-17 02:26:42 +0900

    launchScreen and initial setting

---
+++ voiceMemo/App/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  voiceMemo
+//
+//  Created by 이준성 on 2/17/26.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // 앱이 시작될 때 실행될 초기화 로직 (예: 푸시 알림 설정 등)
+        return true
+    }
+}
--- voiceMemo/App/voiceMemoApp.swift
+++ voiceMemo/App/voiceMemoApp.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @main
 struct voiceMemoApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
