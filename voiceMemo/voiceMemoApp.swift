277dfb45e28f06ddf128648ee818e97b830c6906
Junsung_Lee <brian9777@naver.com>
2026-02-17 00:16:37 +0900

    Initial Commit

---
+++ voiceMemo/voiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceMemoApp.swift
+//  voiceMemo
+//
+//  Created by 이준성 on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct voiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
